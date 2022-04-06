8aaf9d64e2ba3a7bc81513d00daa94f95765df03
sergeimelnik1 <sergeimelnik-1@yandex.ru>
2022-04-06 10:55:28 +0300

    Второй учебник готов

--- Open Weather UI Learn/Open Weather UI Learn/LoginFormController.swift
+++ Open Weather UI Learn/Open Weather UI Learn/LoginFormController.swift
@@ -8,22 +8,124 @@
 import UIKit
 
 class LoginFormController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBAction func loginButtonPressed(_ sender: Any) {
+            // получаем текст логина
+            let login = loginInput.text!
+            // получаем текст пароль
+            let password = passwordInput.text!
+            
+            // проверяем верны ли они
+            if login == "admin" && password == "123456" {
+                print("успешная авторизация")
+            } else {
+                print("неуспешная авторизация")
+            }
+        }
+//    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
+//
+//    }
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+            super.viewDidLoad()
+         
+            // жест нажатия
+            let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(self.hideKeyboard))
+            // присваиваем его UIScrollVIew
+            scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        }
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+            // Проверяем данные
+            let checkResult = checkUserData()
+            
+            // если данные неверны, покажем ошибку
+            if !checkResult {
+                showLoginError()
+            }
+            
+            // вернем результат
+            return checkResult
+        }
+        
+        func checkUserData() -> Bool {
+            let login = loginInput.text!
+            let password = passwordInput.text!
+            
+            if login == "admin" && password == "123456" {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        func showLoginError() {
+            // Создаем контроллер
+            let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+            // Создаем кнопку для UIAlertController
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            // Добавляем кнопку на UIAlertController
+            alter.addAction(action)
+            // показываем UIAlertController
+            present(alter, animated: true, completion: nil)
+        }
 
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            // Подписываемся на два уведомления, одно приходит при появлении клавиатуры
+            NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIWindow.keyboardWillShowNotification, object: nil)
+            // Второе когда она пропадает
+            NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIWindow.keyboardWillHideNotification, object: nil)
+        // жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(self.hideKeyboard))
+        // присваиваем его UIScrollVIew
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+
+        }
+    //отписываемся
+    override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            
+        NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardWillHideNotification, object: nil)
+        }
+    @objc func hideKeyboard() {
+            self.scrollView?.endEditing(true)
+        }
+
+    // когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        
+        // получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        // добавляем отступ внизу UIScrollView равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // устанавливаем отступ внизу UIScrollView равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
